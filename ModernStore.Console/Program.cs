970deed35b6faeb891bf2a89f35e458103ec92e9
Diego <dondesigner@hotmail.com>
2017-06-01 23:53:40 -0300

    Montando Customer

---
+++ ModernStore.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using ModernStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("Diego", "Fernandes", new DateTime(1980, 12, 24), "1234", "diego@mail.com");
+
+            customer.Activate();
+
+            Console.WriteLine($"{customer.FirstName} {customer.LastName} Está ativo:{customer.Active.ToString()}");
+            Console.ReadKey();
+        }
+
+     
+    }
+}
---
+++ ModernStore.Domain/Entities/Customer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernStore.Domain.Entities
+{
+    public class Customer
+    {
+        public Customer( string firstName, string lastName, DateTime? birthDate, string password, string email)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Active = false;
+            UserName = email;
+            Password = password;
+            Email = email;
+
+            //validações
+        }
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public bool Active { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+
+
+        public void Activate()
+        {
+            //Validações
+            Active = true;
+        }
+    }
+}
