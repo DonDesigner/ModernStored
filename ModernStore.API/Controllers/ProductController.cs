13e4c491d1b098a02f83874c5e6d487b50a0dec7
Diego <dondesigner@hotmail.com>
2017-07-02 19:25:52 -0300

    Rotas e Versionamentos de rotas

---
+++ ModernStore.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernStore.API.Controllers
+{
+    //[Route("api")]
+    public class ProductController : Controller
+    {
+
+        [HttpGet]
+        [Route("v1/products")]
+        public string Get()
+        {
+            return $"Listando todos os produtos";
+        }
+
+
+        [HttpGet]
+        [Route("v1/products/{id}")]
+        public string Get(Guid id)
+        {
+            return $"Produto {id}!";
+        }
+
+        [HttpPost]
+        [Route("v1/products")]
+        public string Post()
+        {
+            return "Método Post";
+        }
+
+    }
+}
